4d30cd962135370a2d472d2f4e21c36ad0e0d641
Patrick Reynolds <cpreynolds@gmail.com>
2013-04-08 15:53:37 -0400

    ENH: [Fixes 47070389] Adding initial pass at DoD PoN parsing.
    There are a few lingering questions about the nature of the actual
    stream. Right now we're assuming single PoN xml files.

--- adapters/__init__.py
+++ adapters/__init__.py
@@ -1,1 +1,6 @@
+#!/usr/bin/evn python
+# -*- coding: utf-8 -*-
+
 from flu import GoogleFluAdapter
+from base import BaseAdapter
+from pon import PointOfNeedDiagnosticAdapter
---
+++ adapters/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/evn python
+# -*- coding: utf-8 -*-
+
+
+class BaseAdapter(object):
+    """This serves as a base class for all Data Adapters in the system."""
+
+    def __init__(self):
+        super(BaseAdapter, self).__init__()
+
+    def adapt(self, data):
+        """Transforms input data into GeoJSON and return that GeoJSON buffer.
+
+        :param data: A string containing the input data to be adapted
+        :returns: A string containing the resulting GeoJSON.
+        """
+        return NotImplemented
