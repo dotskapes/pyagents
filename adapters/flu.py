da792bf152e29afeeabea9ea1aa3d7e6218a8741
Patrick Reynolds <cpreynolds@gmail.com>
2013-04-09 15:58:34 -0400

    ENH: [Fixes 47070389] Fixing style issue and adding license header.

--- adapters/flu.py
+++ adapters/flu.py
@@ -1,3 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#
+# Library:   pyagents
+#
+# Copyright 2013 Canepi Team
+#
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 ( the "License" );
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###############################################################################
+
 import os
 import inspect
 
@@ -8,7 +33,10 @@ except ImportError:
 
 import codecs
 
-class GoogleFluAdapter:
+from pyagents.base import BaseAdapter
+
+
+class GoogleFluAdapter(BaseAdapter):
     def __init__(self):
         self.input = 'csv'
         self.output = 'json'
@@ -16,7 +44,8 @@ class GoogleFluAdapter:
         self.geomLookup = {}
 
         path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-        referenceData = unicode(codecs.open(path + '/../data/admin1.json', encoding = 'latin-1',  mode = 'r').read ())
+        referenceData = unicode(codecs.open(path + '/../data/admin1.json',
+                                encoding='latin-1',  mode='r').read())
         referenceGeom = json.loads(referenceData)
         for feature in referenceGeom['features']:
             name = feature['properties']['name']
@@ -25,18 +54,15 @@ class GoogleFluAdapter:
                 if len(alt):
                     self.geomLookup[alt] = feature['geometry']
 
-        
     def adapt(self, data):
-        lines = data.split ('\n')[11:]
+        lines = data.split('\n')[11:]
         adminNames = lines[0].split(',')
         adminAttr = {}
         features = []
 
         for admin in adminNames:
             if admin in self.geomLookup:
-                adminAttr[admin] = {
-                    'name': admin
-                    }
+                adminAttr[admin] = {'name': admin}
                 print admin
 
         for line in lines[1:]:
@@ -50,11 +76,9 @@ class GoogleFluAdapter:
             feature = {
                 'type': 'Feature',
                 'properties': adminAttr[admin],
-                'geometry': self.geomLookup[admin]
-                }
+                'geometry': self.geomLookup[admin]}
             features.append(feature)
         buffer = json.dumps({
-                'type': 'FeatureCollection',
-                'features': features 
-                });
+            'type': 'FeatureCollection',
+            'features': features})
         return buffer
