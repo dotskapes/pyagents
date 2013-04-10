2ae06aeeab119f0d5fa089acaaafd00902b9f78e
Yuzheng Zhou <yuzheng.zhou@kitware.com>
2013-04-10 13:14:08 -0400

    Add healthmap adpater to get last 7 days alerts.

---
+++ pyagents/adapters/healthmap.py
@@ -0,0 +1,70 @@
+
+###############################################################################
+#
+# Library:   pyagents
+#
+# Copyright 2013 Canepi Team
+#
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 ( the "License" );
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###############################################################################
+
+import urllib2
+
+from pprint import pprint
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+from pyagents.adapters import BaseAdapter
+
+class HealthMapAdapter(BaseAdapter):
+    """Data adapter for handling the Healthmap data"""
+
+    def __init__(self):
+        super(HealthMapAdapter, self).__init__()
+
+    def adapt(self, data):
+        auth = ''
+        feature_collection = []
+        target_url = 'http://healthmap.org/HMapi.php?auth=' + auth + '&striphtml=1'
+        data = json.load(urllib2.urlopen(target_url))
+       
+        for record in data:
+            loc = [record['lat'], record['lng']]
+            properties = dict()
+            properties['country'] = record['country']
+            properties['place_name'] = record['place_name']
+            for alert in record['alerts']:
+                properties['date'] = alert['date']
+                properties['descr'] = alert['descr']
+                properties['disease'] = alert['disease']
+                properties['feed'] = alert['feed']
+                properties['link'] = alert['link']
+                properties['rating'] = alert['rating']
+                properties['summary'] = alert['summary']
+                # change data into geojson format
+                geometry = {'type': 'Point',
+                          'coordinates': loc}
+                feature = {'type': 'Feature',
+                         'geometry': geometry,
+                         'properties': properties}
+                feature_collection.append(feature)
+        return json.dumps({'type': 'FeatureCollection',
+                           'features': [feature_collection]})
+
+
+
